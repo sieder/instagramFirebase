4a9ef1fdd74e18af6ad88fd8f1ba1843420d5008
Sieder Villareal <sieder00@gmail.com>
2018-01-16 23:10:05 +0300

    Initial Commit

---
+++ instagramFirebase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  instagramFirebase
+//
+//  Created by sieder on 1/16/18.
+//  Copyright © 2018 sieder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
