edd379662ddb204f7bbe2d199d4f2f2d92578188
Sieder Villareal <sieder00@gmail.com>
2018-02-03 08:01:42 +0300

    nav and tab bar

---
+++ instagramFirebase/Utils/Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  Extensions.swift
+//  instagramFirebase
+//
+//  Created by sieder on 1/17/18.
+//  Copyright © 2018 sieder. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+        
+    }
+    
+    
+}
